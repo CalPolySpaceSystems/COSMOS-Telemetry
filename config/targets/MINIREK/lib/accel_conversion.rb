ac929c0e9b154678b40bf5e301fcecb0bbb0e814
pchizek <patrick.chizek@comcast.net>
2019-04-06 13:59:26 -0700

    Tayo and Eric modifying the packet reading functions in COSMOS to handle the new packet from the IMU. Noteably, we are working on the conversion scripts. This code is not currently working.

--- config/targets/MINIREK/lib/accel_conversion.rb
+++ config/targets/MINIREK/lib/accel_conversion.rb
@@ -5,8 +5,36 @@ require 'cosmos/conversions/conversion'
 
 module Cosmos
   class AccelConversion < Conversion
+    def initialize(saturation_pos)
+      super()
+      @saturation_pos = saturation_pos
+    end        
     def call(value, packet, buffer)
-		  return (value*2.0) / 32768.0	
+      if saturation_pos == 1
+        # check x saturation
+        # define conversion factor
+      else if saturation_pos == 2
+        # check y sautration
+        # define conversion factor
+      else
+        # check z saturation
+        # define conversion factor
+		  return value * #conversion factor	
     end
   end
 end
+
+#OLD CODE
+
+# Implements a conversion factor based on the MPXH6400A pressure sensor spec
+# The conversion assumes in input voltage of 5.0V and outputs in kPa
+
+#require 'cosmos/conversions/conversion'
+
+#module Cosmos
+#  class AccelConversion < Conversion       
+#    def call(value, packet, buffer)
+#		  return (value*2.0) / 32768.0	
+#    end
+#  end
+#end
